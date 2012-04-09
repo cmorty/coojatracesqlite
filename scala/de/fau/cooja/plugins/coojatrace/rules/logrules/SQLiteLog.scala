18c180d75b721225d7bd1afb18ce7c7e033e7150
Moritz 'Morty' Strübe <morty@gmx.net>
2012-04-09 17:17:33 +0200

    Move to actors

--- scala/de/fau/cooja/plugins/coojatrace/rules/logrules/SQLiteLog.scala
+++ scala/de/fau/cooja/plugins/coojatrace/rules/logrules/SQLiteLog.scala
@@ -28,6 +28,10 @@ package de.fau.cooja.plugins.coojatrace.rules.logrules
 
 
 import se.sics.cooja._
+import se.sics.cooja.Simulation._
+import scala.actors._
+import scala.actors.Actor._
+import scala.concurrent.Lock
 
 import de.fau.cooja.plugins.coojatrace._
 import rules._
@@ -48,26 +52,117 @@ class SQLiteLog
  */
 package sqlitelog {
 
+  case class PrepareStmntQuery(query : String)
+  case class PrepareStmnt(stmt : SQLiteStatement)
+  
+  case object Close
+  case object Commit
+  
 /**
  * Wrapper class for a SQLite database.
  *
  * @param file SQLite database filename
  * @param sim current simulation
  */
-case class SQLiteDB(file: String)(implicit sim: Simulation) {
+case class SQLiteDB(file: String)(implicit sim: Simulation) extends Actor {
+   
+     
+    /**
+   * Logger.
+   */
+  val log = org.apache.log4j.Logger.getLogger(this.getClass)
+  
+  val lock = new Lock () 
+  
+  log.info("Created DB-Object: " + file)
+  
+  start()
+    
   /**
    * DB connection. Lazily initialized to ensure this is called from the simulation thread
    * as the sqlite wrapper is not thread safe and refuses to work if initialized from another thread.
    */
-  lazy val connection: SQLiteConnection = {
+  var active = false
+  var forceflush = false
+  
+  val flush = 1000
+  
+  
+
+  
+  
+  def act() {
+    sim.addObserver(new Observer() {
+		def update(obs: Observable, obj: Object) {
+			if(!sim.isRunning){
+			   send(Commit, Actor.self) 
+			} 
+		}
+	})
+	
+	log.info("Started DB-Actor")
+    var uncommit = 0
+    
+    connection
+	while(active){
+		if( uncommit >= flush || forceflush){		 
+		    log.info("Flushing " + uncommit + " entries")
+		    uncommit = 0;
+		    forceflush = false
+		    connection.exec("COMMIT")
+		    connection.exec("BEGIN")
+		}
+	  
+		receive {
+          case str:String => connection.exec(str)          
+          case Close => active = false 
+          case Commit => forceflush = true
+          case prep:PrepareStmntQuery => 
+            sender ! PrepareStmnt(connection.prepare(prep.query, true))
+          case stmt:PrepareStmnt =>
+            stmt.stmt.step
+            stmt.stmt.reset
+            uncommit += 1
+            lock.release()
+          case _ => {println("Something went wrong here!")}
+		}
+    }
+    active = false
+    connection.exec("COMMIT")
+    connection.dispose
+    log.info("Stopped DB-Actor")
+    
+  }
+    
+   
+  
+  private lazy val connection: SQLiteConnection = {
+    log.info("Init CON:" + file.toString() + "  Obj:" + this.toString + " Hash:" + this.hashCode)
+    val mthis = this
+
     // close db on plugin deactivation
     CoojaTracePlugin.forSim(sim).onCleanUp {
-      connection.dispose()
+	    this ! Close
     }
     
+    sim.addObserver(new Observer() {
+		def update(obs: Observable, obj: Object) {
+			if(!sim.isRunning){
+			   mthis ! Commit 
+			} 
+		}
+	})
+    
     // opens or create db
-    new SQLiteConnection(new java.io.File(file)).open(true).exec("BEGIN")
+    val conn = new SQLiteConnection(new java.io.File(file)).open(true)
+    conn.exec("PRAGMA synchronous = OFF")
+    conn.exec("BEGIN")
+    active = true
+    conn
   }
+  
+  
+  
 }
 
 /**
@@ -82,13 +177,16 @@ case class SQLiteDB(file: String)(implicit sim: Simulation) {
  */
 case class LogTable(db: SQLiteDB, table: String, columns: List[String], timeColumn: String = "Time")(implicit sim: Simulation) extends LogDestination {
   // active as long as queue is running (i.e. db connection is open)
-  def active = db.connection.isOpen
-  
+  def active = db.active
+  implicit def string2PrepareStmntQuery(query : String) = new PrepareStmntQuery(query)
+
   /**
    * Logger.
    */
   val logger = org.apache.log4j.Logger.getLogger(this.getClass)
 
+  logger.info("Created Table-Object: " + table)
+  
   /**
    * Complete columns list (time added if not disabled).
    */
@@ -99,63 +197,56 @@ case class LogTable(db: SQLiteDB, table: String, columns: List[String], timeColu
    */
   val colNames = allColumns.map(_.replace(" ", "_"))
 
-
-  var uncommitedInserts = 0
-  var init = false
-
-  def commit() {
-    db.connection.exec("COMMIT")
-    db.connection.exec("BEGIN")
-    uncommitedInserts = 0
-  }
+  var lockctr = 0
+  
+  
 
   // recreate table, start transaction and save prepared INSERT statement
   // this is lazy to create (lazy) db for reasons above
-  lazy val insertStatement = {
-	if(!init){
-		db.connection.exec("DROP TABLE IF EXISTS " + table)
-		db.connection.exec("CREATE TABLE " + table + colNames.mkString("(", ", ", ")"))
-		logger.info("Created table " + table)
-		commit()
-		init = true;
-	}
-    db.connection.prepare("INSERT INTO " + table + colNames.mkString("(", ", ", ")") +
-                          " VALUES " + colNames.map(c => "?").mkString("(", ", ", ")"), true)
+  private lazy val insertStatement:PrepareStmnt = {
+    db ! "DROP TABLE IF EXISTS " + table
+    db ! "CREATE TABLE " + table + colNames.mkString("(", ", ", ")")
+    logger.info("Creating Table " + table)
+    db ! Commit
+    db ! "INSERT INTO " + table + colNames.mkString("(", ", ", ")") +
+                          " VALUES " + colNames.map(c => "?").mkString("(", ", ", ")")
+    receiveWithin(10000){
+      case rep:PrepareStmnt => 
+      	CoojaTracePlugin.forSim(sim).onCleanUp {
+      		rep.stmt.dispose
+      	}
+        rep
+      case TIMEOUT => throw new UnsupportedOperationException("Unable to get prepared statement!"); 
+    }
   }
 
-  // add observer to Simulation which commits transaction when sim is stopped
-  sim.addObserver(new Observer() {
-    def update(obs: Observable, obj: Object) {
-      if(!sim.isRunning) commit()
-    }
-  })
 
-  def log(values: List[_]) {
-    // check for right number of columns
-    require(values.size == columns.size, "incorrect column count")
 
-    try {
-      // bind value (and time if not disabled) to insert statement
-      val start = if(timeColumn != null) {
-        insertStatement.bind(1, sim.getSimulationTime)
-        2 // bind values from seconds on
-      } else {
-        1 // bind values from first on
-      }
-      for((v, i) <- values.zipWithIndex) insertStatement.bind(i+start, v.toString)
-      
-      // execute statement
-      insertStatement.step()
+  def log(values: List[_]) {	  
+      // check for right number of columns
+	  require(values.size == columns.size, "incorrect column count")
+    
+	  if (!db.lock.available){
+	    lockctr += 1
+	    if(lockctr == 10){
+	      logger.warn("The Database is slowing us down")
+	    }
+	  }
+	  db.lock.acquire()
+	  
+	  // bind value (and time if not disabled) to insert statement
+	  val start = if(timeColumn != null) {
+	    insertStatement.stmt.bind(1, sim.getSimulationTime)
+	    2 // bind values from seconds on
+	  } else {
+	    1 // bind values from first on
+	  }
+	  for((v, i) <- values.zipWithIndex) insertStatement.stmt.bind(i+start, v.toString)
+	  
+	  // execute statement
+	  db ! insertStatement
 
-      // commit every 10000 inserts
-      uncommitedInserts += 1
-      if(uncommitedInserts >= 10000) commit()
-    } catch {
-      case e: Exception => logger.error("DB exception: " + e)
-    } finally {
-      // reset bindings for next call
-      insertStatement.reset()
-    }
+    
   }
 }
 
